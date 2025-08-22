da1a2be9ba55dfb1f1392bda00c510b644892b23
Fábio Matos <gleek.rj@gmail.com>
2025-08-22 18:16:50 -0300

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { desc } from "drizzle-orm";
 import Image from "next/image";
 import React from "react";
 
 import CategorySelector from "@/components/common/category-selector";
+import Footer from "@/components/common/footer";
 import { Header } from "@/components/common/header";
 import ProductList from "@/components/common/product-list";
 import { db } from "@/db";
+import { productTable } from "@/db/schema";
 
 const Home = async () => {
   const products = await db.query.productTable.findMany({
@@ -12,8 +15,15 @@ const Home = async () => {
       variants: true,
     },
   });
+  const newlyCreatedProducts = await db.query.productTable.findMany({
+    orderBy: [desc(productTable.createAt)],
+    with: {
+      variants: true,
+    },
+  });
 
   const categories = await db.query.categoryTable.findMany({});
+
   return (
     <>
       <Header />
@@ -45,6 +55,9 @@ const Home = async () => {
             className="h-auto w-full"
           />
         </div>
+
+        <ProductList title="Novos Produtos" products={newlyCreatedProducts} />
+        <Footer />
       </div>
     </>
   );
