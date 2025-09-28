a65ec7487fefb35a81ed762f5b19d11cb3c644fb
Fábio Matos <gleek.rj@gmail.com>
2025-09-28 11:14:32 -0300

    feat: add checkout success page

--- src/app/cart/confirmation/components/finish-order-button.tsx
+++ src/app/cart/confirmation/components/finish-order-button.tsx
@@ -1,19 +1,13 @@
 "use client";
 
-import { DialogDescription, DialogTitle } from "@radix-ui/react-dialog";
 import { loadStripe } from "@stripe/stripe-js";
 import { Loader2 } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
-import { useState } from "react";
 
 import { createCheckoutSession } from "@/action/create-checkout-session";
 import { useFinishOrder } from "@/app/hooks/mutations/use-finish-order";
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogFooter } from "@/components/ui/dialog";
 
 const FinishOrderButton = () => {
-  const [sucessDialogIsOpen, setSucessDialogIsOpen] = useState(false);
   const finishOrderMutation = useFinishOrder();
   const handleFinishOrderClick = async () => {
     if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
@@ -31,7 +25,6 @@ const FinishOrderButton = () => {
     await stripe.redirectToCheckout({
       sessionId: checkoutSession.id,
     });
-    setSucessDialogIsOpen(true);
   };
 
   return (
@@ -47,30 +40,6 @@ const FinishOrderButton = () => {
         )}
         Finalizar Compra
       </Button>
-      <Dialog open={sucessDialogIsOpen} onOpenChange={setSucessDialogIsOpen}>
-        <DialogContent className="text-center">
-          <Image
-            src="/illustration.svg"
-            alt="Sucesso"
-            width={300}
-            height={300}
-            className="mx-auto"
-          />
-          <DialogTitle className="mt-4 text-2xl">Pedido efetuado!</DialogTitle>
-          <DialogDescription className="font-medium">
-            Seu pedido foi efetuado com sucesso. Você pode acompanhar o status
-            na seção de “Meus Pedidos”.
-          </DialogDescription>
-          <DialogFooter>
-            <Button className="w-full rounded-full" size="lg">
-              Ver meus pedidos
-            </Button>
-            <Button className="w-full rounded-full" size="lg" variant="outline">
-              <Link href="/">Voltar para a loja</Link>
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
     </>
   );
 };
